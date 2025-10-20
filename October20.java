810fdeef6f473b9bf076a3265bb8a5bf9f3406d1
Finian Pasterczyk <finian.pasterczyk@gmail.com>
2025-10-20 09:09:48 -0700

    Update October20.java
    changed # to $

--- October20.java
+++ October20.java
@@ -2,7 +2,7 @@ class October20 {
    public static void main(String[] args) {
       for (int i = 0; i < 100; i++) {
          for (int j = 0; j < 10 - (int) (Math.cos(i*0.5)*10); j++) {
-            System.out.print("#");
+            System.out.print("$");
          }
          System.out.println("");
       }
