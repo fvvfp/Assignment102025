9e623fccb47f3510d4fa9b2aa9b61b8951f46bb6
Finian Pasterczyk <finian.pasterczyk@gmail.com>
2025-10-21 08:56:21 -0700

    Changed name to meet assignment requirements

--- Assignment102025.java
+++ Assignment102025.java
@@ -1,4 +1,4 @@
-class October20 {
+class Assignment102025 {
    public static void main(String[] args) {
       for (int i = 0; i < 100; i++) {
          for (int j = 0; j < 10 - (int) (Math.cos(i*0.5)*10); j++) {
