858c2da2621e5bc4787c08afb50e4d0b9cc964b5
Finian Pasterczyk <finian.pasterczyk@gmail.com>
2025-10-21 09:03:15 -0700

    $ -> # to test change for assignment

--- Assignment102025.java
+++ Assignment102025.java
@@ -2,7 +2,7 @@ class Assignment102025 {
    public static void main(String[] args) {
       for (int i = 0; i < 100; i++) {
          for (int j = 0; j < 10 - (int) (Math.cos(i*0.5)*10); j++) {
-            System.out.print("$");
+            System.out.print("#");
          }
          System.out.println("");
       }
